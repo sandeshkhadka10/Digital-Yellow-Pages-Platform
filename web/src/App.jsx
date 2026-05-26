074d91c3addf66c12330eaa89199533d4ba7ba0f
sandeshkonnectcraft <sandesh.khadka@konnectcraft.com>
2026-05-26 15:08:51 +0545

    install the necessary package for web

---
+++ web/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline text-center text-blue-500'>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
